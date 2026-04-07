6d469049834a9fd4dfa70efba3092f4c1879369e
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2026-04-07 16:06:43 +0400

    fix: Hazelcast client name is reverted in HazelcastInstanceConfig. Project version is changed to 5.0.11-SNAPSHOT.

--- src/main/java/org/qubership/automation/itf/core/util/hazelcast/instance/HazelcastInstanceConfig.java
+++ src/main/java/org/qubership/automation/itf/core/util/hazelcast/instance/HazelcastInstanceConfig.java
@@ -54,9 +54,6 @@ public class HazelcastInstanceConfig {
     @Value("${hazelcast.cluster-name}")
     private String clusterName;
 
-    @Value("${hazelcast.client.name}")
-    private String hazelcastClientName;
-
     /**
      * Create Config object, populate its properties and return the object.
      *
@@ -65,7 +62,7 @@ public class HazelcastInstanceConfig {
     @Bean(name = "instanceConfig")
     public Config getConfig() {
         Config config = new Config();
-        config.setInstanceName(hazelcastClientName);
+        config.setInstanceName(HIBERNATE_CACHE_HAZELCAST_INSTANCE_NAME.stringValue());
         if (hazelcastCacheEnabled) {
             config.setProperty("hazelcast.phone.home.enabled", "false");
             config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
