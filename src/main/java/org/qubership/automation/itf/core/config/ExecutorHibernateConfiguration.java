606b564d101d684f34dba4827e2bd79a20de2b5f
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2026-05-05 17:16:27 +0400

    fix: Configuration of Hibernate L2 Cache via Hazelcast is fixed. Project version is set to 5.1.5-SNAPSHOT.

--- src/main/java/org/qubership/automation/itf/core/config/ExecutorHibernateConfiguration.java
+++ src/main/java/org/qubership/automation/itf/core/config/ExecutorHibernateConfiguration.java
@@ -68,25 +68,25 @@ public class ExecutorHibernateConfiguration {
     public FactoryBean<EntityManagerFactory> getLocalContainerEntityManagerFactoryBean(
             DataSource dataSource,
             Properties jpaProperties,
-            @Qualifier("hazelcastCacheInstance") HazelcastInstance hazelcastInstance) {
-        return createEntityManagerFactory(dataSource, jpaProperties, hazelcastInstance);
+            @Qualifier("hazelcastCacheInstance") HazelcastInstance hazelcastCacheInstance) {
+        return createEntityManagerFactory(dataSource, jpaProperties, hazelcastCacheInstance);
     }
 
     /**
      * Constructor of EntityManagerFactory in case hazelcastCacheInstance doesn't exist.
      */
-    /*@Bean(name = "entityManagerFactory")
+    @Bean(name = "entityManagerFactory")
     @ConditionalOnMissingBean(name = "hazelcastCacheInstance")
     public FactoryBean<EntityManagerFactory> getLocalContainerEntityManagerFactoryBeanWithoutHazelcast(
             DataSource dataSource,
             Properties jpaProperties) {
         return createEntityManagerFactory(dataSource, jpaProperties, null);
-    }*/
+    }
 
     private FactoryBean<EntityManagerFactory> createEntityManagerFactory(
             DataSource dataSource,
             Properties jpaProperties,
-            HazelcastInstance hazelcastInstance) {
+            HazelcastInstance hazelcastCacheInstance) {
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
         emf.setPackagesToScan("org.qubership.automation.itf.core.model.jpa");
         emf.setDataSource(dataSource);
@@ -110,13 +110,13 @@ public class ExecutorHibernateConfiguration {
                 "mapping/EntitiesMigration.hbm.xml");
 
         log.info("createEntityManagerFactory: secondLevelCacheEnabled {}", secondLevelCacheEnabled);
-        if (hazelcastInstance != null && secondLevelCacheEnabled) {
+        if (hazelcastCacheInstance != null && secondLevelCacheEnabled) {
             // 1. JCache provider configuring
             System.setProperty("hazelcast.jcache.provider.type", "member");
             CachingProvider provider = new HazelcastCachingProvider();
 
             // 2. Please use EXISTING HazelcastInstance by name
-            Properties props = HazelcastCachingProvider.propertiesByInstanceName(hazelcastInstance.getName());
+            Properties props = HazelcastCachingProvider.propertiesByInstanceName(hazelcastCacheInstance.getName());
             props.setProperty("hazelcast.jcache.provider.type", "member");
 
             CacheManager manager = provider.getCacheManager(URI.create("hibernate-l2-cache"), null, props);
@@ -127,7 +127,7 @@ public class ExecutorHibernateConfiguration {
             jpaProperties.put("hibernate.javax.cache.uri", "hibernate-l2-cache");
             //jpaProperties.put("hibernate.javax.cache.cache_manager", manager);
 
-            log.info("createEntityManagerFactory: hazelcastInstance '{}' is used", hazelcastInstance.getName());
+            log.info("createEntityManagerFactory: hazelcastInstance '{}' is used", hazelcastCacheInstance.getName());
         } else {
             log.info("createEntityManagerFactory: hazelcastInstance is null or cache disabled");
         }
