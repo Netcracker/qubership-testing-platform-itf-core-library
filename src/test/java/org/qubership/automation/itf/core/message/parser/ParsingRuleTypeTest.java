a5b5ad473745f5efa6ca5e60f59f96e8252ef402
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2026-07-23 00:10:15 +0400

    fix(tests): Fix ParsingRuleTypeTest Unit tests. Set explicit version of jdom2 dependency. Set project version to 5.1.27-SNAPSHOT.

--- src/test/java/org/qubership/automation/itf/core/message/parser/ParsingRuleTypeTest.java
+++ src/test/java/org/qubership/automation/itf/core/message/parser/ParsingRuleTypeTest.java
@@ -16,10 +16,10 @@
 
 package org.qubership.automation.itf.core.message.parser;
 
-import static org.qubership.automation.itf.core.util.parser.ParsingRuleType.XPATH;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.qubership.automation.itf.core.util.parser.ParsingRuleType.XPATH;
 
 import java.io.File;
 import java.io.IOException;
@@ -29,17 +29,17 @@ import java.util.concurrent.Executors;
 
 import org.jdom2.Element;
 import org.junit.Test;
-import org.testng.reporters.Files;
-
 import org.qubership.automation.itf.core.model.content.Content;
 import org.qubership.automation.itf.core.model.jpa.message.Message;
 import org.qubership.automation.itf.core.model.jpa.message.parser.MessageParameter;
 import org.qubership.automation.itf.core.model.jpa.message.parser.ParsingRule;
 import org.qubership.automation.itf.core.model.jpa.message.parser.SystemParsingRule;
+import org.qubership.automation.itf.core.model.jpa.system.System;
 import org.qubership.automation.itf.core.util.exception.ContentException;
 import org.qubership.automation.itf.core.util.helper.ContentHelper;
 import org.qubership.automation.itf.core.util.parser.ParsingRuleType;
 import org.qubership.automation.itf.core.util.provider.content.XmlContentProvider;
+import org.testng.reporters.Files;
 
 public class ParsingRuleTypeTest {
 
@@ -53,7 +53,7 @@ public class ParsingRuleTypeTest {
             + "\"mac_address\",\"value\":\"4f:25:12:15:ab:f5\"}]}]}]}}";
 
     @Test
-    public void testParsingJsonPath() throws InterruptedException {
+    public void testParsingJsonPath() {
         ParsingRule parsingRule = mock(ParsingRule.class);
         when(parsingRule.getMultiple()).thenReturn(true);
         when(parsingRule.getExpression()).thenReturn("body.serviceModelName");
@@ -79,10 +79,11 @@ public class ParsingRuleTypeTest {
         ParsingRule parsingRule = mock(SystemParsingRule.class);
         when(parsingRule.getMultiple()).thenReturn(false);
         when(parsingRule.getExpression()).thenReturn("(.*)");
+        when(parsingRule.getParsedExpression()).thenReturn("(.*)");
         when(parsingRule.getParsingType()).thenReturn(ParsingRuleType.REGEX_URI);
         when(parsingRule.getParamName()).thenReturn("param");
         Message message = new Message("");
-        String er = "http://itf-virginmedia.openshift.qubership.cloud/#/system/975";
+        String er = "http://example.com/account/create";
         message.getConnectionProperties().put("uriParams", er);
         MessageParameter parse = ParsingRuleType.REGEX_URI.parse(message, parsingRule);
         String value = parse.getSingleValue();
@@ -93,8 +94,10 @@ public class ParsingRuleTypeTest {
     public void testParsingXpath() throws URISyntaxException, IOException, ContentException {
         File file = new File(getClass().getResource("/parsing_rule/xml_expression.xml").toURI());
         String xmlContent = Files.readFile(file);
+        String xpath = "//*[local-name() = 'UPRN']/text()"; // "//UPRN/text()" - it works only w/o namespace
         ParsingRule parsingRule = mock(SystemParsingRule.class);
-        when(parsingRule.getExpression()).thenReturn("//UPRN/text()");
+        when(parsingRule.getExpression()).thenReturn(xpath);
+        when(parsingRule.getParsedExpression()).thenReturn(xpath);
         when(parsingRule.getParsingType()).thenReturn(XPATH);
         when(parsingRule.getMultiple()).thenReturn(true);
         Message message = new Message(xmlContent);
@@ -102,5 +105,71 @@ public class ParsingRuleTypeTest {
         message.setContent(provide);
         MessageParameter parse = XPATH.parse(message, parsingRule);
         assertEquals("900000002319", parse.getSingleValue());
+        assertEquals(2, parse.getMultipleValue().size());
+    }
+
+    @Test
+    public void testParsingXpath_getOperation() throws ContentException {
+        String xml = """
+                <soapenv:Envelope
+                    xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                    xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">
+                    <soapenv:Header/>
+                    <soapenv:Body>
+                        <CheckEligibilityServiceRequestMessage
+                            xmlns="http://zain.com/telecom/esb/xsd/checkeligibilityservice/verifyusereligibility/request/v1_0_0_0"
+                            xmlns:ns0="http://zain.com/telecom/esb/xsd/SecurityManagement/common/headers/v1_0_0_0">
+                            <RequestHeader>
+                                <ns0:Timestamp>2023-01-12T10:05:09.169+03:00</ns0:Timestamp>
+                                <ns0:ChannelId>NCCSRD</ns0:ChannelId>
+                                <ns0:ChannelTransctionId>571a7b94-f745-447a-b1fb-0971ec9a097d</ns0:ChannelTransctionId>
+                                <ns0:Auth_key>YWRtaW4=</ns0:Auth_key>
+                            </RequestHeader>
+                            <CheckEligibilityServiceRequestPayload>
+                                <requestType>1</requestType>
+                                <Operator>
+                                    <employeeIdType>1</employeeIdType>
+                                    <operatorTCN>fabd81c5-bb81-46a9-9bcc-de402ba5837f</operatorTCN>
+                                    <EmployeeId>1233329999</EmployeeId>
+                                </Operator>
+                                <Person>
+                                    <personId>{v2c}{AES/ECB/PKCS5Padding}{default-key-alias_1646810792369}{e3uYmrz/CLAH+uQu4Ybi/g==}</personId>
+                                    <nationality>{v2c}{AES/ECB/PKCS5Padding}{default-key-alias_1646810792369}{fFcvMytR+/w1uwpFvGyCqQ==}</nationality>
+                                    <IdType>1</IdType>
+                                    <exceptionflag>0</exceptionflag>
+                                </Person>
+                                <Mobile>
+                                    <subscriptionType>0</subscriptionType>
+                                    <msisdnType>V</msisdnType>
+                                </Mobile>
+                            </CheckEligibilityServiceRequestPayload>
+                        </CheckEligibilityServiceRequestMessage>
+                    </soapenv:Body>
+                </soapenv:Envelope>""";
+        Message message = new Message(xml);
+        Content<Element> provide = new XmlContentProvider().provide(message);
+        message.setContent(provide);
+
+        String xpath = "local-name(/*[local-name() = 'Envelope']/*[local-name() = 'Body']/*[1])";
+
+        ParsingRule<System> parsingRule = mock(SystemParsingRule.class);
+        when(parsingRule.getExpression()).thenReturn(xpath);
+        when(parsingRule.getParsedExpression()).thenReturn(xpath);
+        when(parsingRule.getParsingType()).thenReturn(XPATH);
+        when(parsingRule.getMultiple()).thenReturn(false);
+        when(parsingRule.getAutosave()).thenReturn(true);
+
+        MessageParameter parameter = XPATH.parse(message, parsingRule);
+        assertEquals("CheckEligibilityServiceRequestMessage", parameter.getSingleValue());
+
+        // Briefly, Xpath parsing is the following sequence of steps:
+        /*
+        SAXBuilder builder = new SAXBuilder();
+        Document doc = builder.build(new StringReader(xml));
+        XPathFactory xpf = XPathFactory.instance();
+        XPathExpression<Object> expr = xpf.compile(xpath);
+        Object result = expr.evaluateFirst(doc);
+        java.lang.System.out.println("Result: " + result);
+         */
     }
 }
