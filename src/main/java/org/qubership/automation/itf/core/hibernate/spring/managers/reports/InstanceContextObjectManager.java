50ad74c4dba342e2a36e7b92c11ac0ae38118610
dmch1019 <dmitrii.churbanov@netcracker.com>
2026-07-28 01:39:25 +0500

    feat: fix cast long to biginteger for report context; change version to 5.1.24-SNAPSHOT;

--- src/main/java/org/qubership/automation/itf/core/hibernate/spring/managers/reports/InstanceContextObjectManager.java
+++ src/main/java/org/qubership/automation/itf/core/hibernate/spring/managers/reports/InstanceContextObjectManager.java
@@ -141,7 +141,8 @@ public class InstanceContextObjectManager extends AbstractObjectManager<Instance
         if (!(Objects.isNull(initiatorInfo) || initiatorInfo.isEmpty())) {
             tcContextBriefInfo.setIniname((String) initiatorInfo.get(0)[0]);
             tcContextBriefInfo.setInitiatortype((String) initiatorInfo.get(0)[1]);
-            tcContextBriefInfo.setSituationId(BigInteger.valueOf((Long) initiatorInfo.get(0)[2]));
+            tcContextBriefInfo.setSituationId(Objects.isNull(initiatorInfo.get(0)[2])
+                    ? null : BigInteger.valueOf((Long) initiatorInfo.get(0)[2]));
             tcContextBriefInfo.setChainId(Objects.isNull(initiatorInfo.get(0)[3])
                     ? null : BigInteger.valueOf((Long) initiatorInfo.get(0)[3]));
             tcContextBriefInfo.setExecutiondata((String) initiatorInfo.get(0)[4]);
